6b247e54f17fb9d71bdce2844904ae3f8c1cf1ad
Rafał Kęsik <kesik.rafal@yahoo.com>
2024-07-24 17:04:58 +0200

    Add menu-bar for mobile view.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 // Opens or closes the menu bar in mobile view.
 function menuBarChangeVisibility() {
     let label = document.querySelector('[for="menu-bar-visible"]');
-    let checkbox = document.querySelector("#menu-bar-visible")
-    console.log(label.innerHTML);
-    console.log(checkbox.checked);
-    
+    let checkbox = document.querySelector("#menu-bar-visible");
+    let iconClassList = label.classList;
+    let dropDownMenu = document.querySelector("#drop-down-menu");
+    // The below one to be deleted, and the above to be uncommented.
+    // let dropDownMenu = document.querySelector("#header-nav-links");
+
     if (checkbox.checked) {
-        // display the menu bar
-        // & Change the icon to "Close" text
+        // dropDownMenu.style.display = 'block';
+        iconClassList.add("highlight_text");
     } else {
-        // hide the menu bar
-        // & Change the "Close" text to icon
+        // dropDownMenu.style.display = 'none';
+        iconClassList.remove("highlight_text");
     }
 }
